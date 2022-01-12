02c2a055df1b1b4b7f0e8e424fc43d401edf8c5d
ant31 <2t.antoine@gmail.com>
2022-01-12 15:44:37 +0100

    Copy to S3 webhook

--- gmail2s3/api/gmail2s3.py
+++ gmail2s3/api/gmail2s3.py
@@ -3,28 +3,28 @@
 # pylint: disable=too-few-public-methods
 import logging
 from copy import deepcopy
-from typing import Tuple
+from typing import Tuple, List, Optional
 from fastapi import APIRouter
 from pydantic import BaseModel, Field
-from gmail2s3.gmailauth import MessageQuery, gmail2s3, MessageList, GmailClient, WebHook
-
-# from gmail2s3.s3 import S3Dest
+from gmail2s3.gmailauth import (
+    MessageQuery,
+    gmail2s3,
+    MessageList,
+    GmailClient,
+    WebHook,
+    WebHookBody,
+)
+from gmail2s3.s3 import S3Dest, S3Client
+from gmail2s3.config import GCONFIG
 
 
 router = APIRouter(prefix="/api/v1", tags=["gmail2s3"])
 logger = logging.getLogger(__name__)
 
 
-class ResponseExample(BaseModel):
-    synced: bool = Field(...)
-    path: str = Field(...)
-    fstat: str = Field(...)
-    request: Item = Field(...)
-
-
 class SyncedEmail(BaseModel):
     message_id: str
-    s3_paths: List[str]
+    s3_paths: Optional[List[str]]
 
 
 class SyncedEmailList(BaseModel):
@@ -38,28 +38,30 @@ class S3Conf(BaseModel):
 
 class SyncedEmailRequest(BaseModel):
     query: Optional[MessageQuery] = Field(MessageQuery())
-    webhooks: Optional[List[WebHook]]
-    s3conf: Optional[S3Conf]
+    webhooks: Optional[List[WebHook]] = Field([])
+    s3conf: Optional[S3Conf] = Field({})
 
 
-@router.post("/sync_emails", response_model=SyncedEmailList)
-async def sync_emails(req: SyncedEmailRequest) -> SyncedEmailList:
+@router.post("/sync_emails", response_model=list[dict])
+async def sync_emails(req: SyncedEmailRequest) -> list[dict]:
     s3conf = deepcopy(GCONFIG.s3)
     if req.s3conf:
         s3conf.update(req.s3conf)
-    return gmail2s3(query=req.query, webhooks=req.webhooks, s3conf=s3conf)
+    return gmail2s3(message_query=req.query, webhooks=req.webhooks, s3conf=s3conf)
 
 
-@router.post("/webhooks/upload_attachment/copy", response_model=Tuple(S3Dest, S3Dest))
+@router.post("/webhooks/upload_attachment/copy", response_model=Tuple[S3Dest, S3Dest])
 async def copy_s3(webhook: WebHookBody):
     s3conf = deepcopy(GCONFIG.s3)
     s3_dest_bucket = webhook.params["s3_copy_dest"]["bucket"]
-    s3_dest_prefix = webhook.params["s3_copy_dest"]["bucket"]
+    s3_dest_prefix = webhook.params["s3_copy_dest"]["prefix"]
     s3_src = webhook.payload.s3_uploads[0]
     s3_client = S3Client(s3conf, bucket=s3conf["bucket"])
-    return s3_client.copy_s3_to_s3(
+    resp = s3_client.copy_s3_to_s3(
         src_bucket=s3_src["bucket"],
         src_path=s3_src["path"],
         dest_bucket=s3_dest_bucket,
         dest_prefix=s3_dest_prefix,
     )
+    logger.info(resp)
+    return resp
